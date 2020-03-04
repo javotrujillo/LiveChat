d58dcd9244a6b844a66c275da50ed80fef565193
Javo Trujillo Lopez <javo_trujillo@outlook.com>
2020-03-04 16:09:42 -0500

    Se crea la version inicial de la pagina web inicial y la pagina del chat.

--- LiveChat/Controllers/HomeController.cs
+++ LiveChat/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace LiveChat.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public Client client { get; set; }
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -23,9 +24,23 @@ namespace LiveChat.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Chat()
         {
-            return View();
+            var fullname = Request.Form["name"].ToString().Split(' ');
+            client = new Client() {
+                firstname = fullname[0],
+                lastname = (fullname.Length > 1 ) ? fullname[1] : "",
+                customerid = Request.Form["customerid"],
+                queuename = Request.Form["queuename"]
+            };
+            //ViewData["name"] = @"Request.Form['name']" + " " + Request.Form["name"];
+            
+            return View(client);
+        }
+
+        public IActionResult LogOut()
+        {
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ LiveChat/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveChat.Models
+{
+    public class Client
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string customerid { get; set; }
+        public string queuename { get; set; }
+        public string addressstreet { get; set; }
+        public string addresscity { get; set; }
+        public string addresspostalcode { get; set; }
+        public string addressState { get; set; }
+        public string phonenumber { get; set; }
+        public string customfield1label { get; set; }
+        public string customfield1 { get; set; }
+        public string customfield2label { get; set; }
+        public string customfield2 { get; set; }
+        public string customfield3label { get; set; }
+        public string customfield3 { get; set; }
+        public string email { get; set; }
+    }
+}
